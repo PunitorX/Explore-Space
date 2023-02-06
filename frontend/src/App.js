6c911d4246b2500d100783da882211bac83faae7
Robert Jones <JonesBrendanDev@gmail.com>
2023-02-06 05:46:38 -0600

    created files and folders; uploaded code and styles

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,48 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+
+// * Page Imports
+import HomePage from './pages/HomePage'
+
+// * Planets
+import Planets from './pages/Planets/Planets'
+import Earth from './pages/Planets/Earth'
+
+// * Stars
+import Stars from './pages/Stars/Stars'
+
+// * Galaxies
+import Galaxies from './pages/Galaxies/Galaxies'
+
+// * Astronomers
+import Astronomers from './pages/Astronomers/Astronomers'
+
+// * Astronomical Objects
+import Objects from './pages/Objects/Objects'
+
+// * Satellites
+import Satellites from './pages/Satellites/Satellites'
 
 function App() {
   return (
-    <>Hello</>
+    <Router>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+
+        <Route path='/planets'>
+          <Route index element={<Planets />} />
+          <Route path='earth' element={<Earth />} />
+        </Route>
+
+        <Route path='/stars' element={<Stars />} />
+        <Route path='/satellites' element={<Satellites />} />
+        <Route path='/Astronomers' element={<Astronomers />} />
+        <Route path='/objects' element={<Objects />} />
+        <Route path='/galaxies' element={<Galaxies />} />
+
+        {/* <Route path='*' element={<NotFound />} /> */}
+      </Routes>
+    </Router>
   );
 }
 
---
+++ frontend/src/pages/HomePage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <div style={{background: '#000', color: '#fff'}}>Home</div>
+  )
+}
+
+export default HomePage
