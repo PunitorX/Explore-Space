1c29af9cf5958d54b87917526b7a86586abef4c6
Robert Jones <jones.brendan1996@gmail.com>
2023-02-07 00:59:28 -0600

    uploaded icons

--- frontend/src/data/SideNavData.js
+++ frontend/src/data/SideNavData.js
@@ -1,45 +1,45 @@
-// import iconOne from '../Icons/astronomer.png'
-// import iconTwo from '../Icons/comet.png'
-// import iconThree from '../Icons/falling-star.png'
-// import iconFour from '../Icons/galaxy.png'
-// import iconFive from '../Icons/planet.png'
-// import iconSix from '../Icons/satellite.png'
+import iconOne from '../Icons/009-galaxy-1.png'
+import iconTwo from '../Icons/023-comet.png'
+import iconThree from '../Icons/035-astronomer.png'
+import iconFour from '../Icons/030-planets.png'
+import iconFive from '../Icons/047-space-exploration.png'
+import iconSix from '../Icons/050-telescope-2.png'
 
 export const NavData = [
   {
     title: 'Galaxies',
     path: '/galaxies',
     cName: 'navItem',
-    // icon: iconOne
+    icon: iconOne
   },
   {
     title: 'Astronomical Object',
     path: '/objects',
     cName: 'navItem',
-    // icon: iconTwo
+    icon: iconTwo
   },
   {
-    title: 'People',
-    path: '/people',
+    title: 'Astronomers',
+    path: '/Astronomers',
     cName: 'navItem',
-    // icon: iconThree
+    icon: iconThree
   },
   {
     title: 'Planets',
     path: '/planets',
     cName: 'navItem',
-    // icon: iconFour
+    icon: iconFour
   },
   {
     title: 'Satellites',
     path: '/satellites',
     cName: 'navItem',
-    // icon: iconFive
+    icon: iconFive
   },
   {
     title: 'Stars',
     path: '/stars',
     cName: 'navItem',
-    // icon: iconSix
+    icon: iconSix
   }
 ]
