05e55ec67888835f41efca0bbb57acdd44c0b155
Robert Jones <jones.brendan1996@gmail.com>
2023-02-06 23:10:00 -0600

    created SideNavData

---
+++ frontend/src/data/SideNavData.js
@@ -0,0 +1,45 @@
+// import iconOne from '../Icons/astronomer.png'
+// import iconTwo from '../Icons/comet.png'
+// import iconThree from '../Icons/falling-star.png'
+// import iconFour from '../Icons/galaxy.png'
+// import iconFive from '../Icons/planet.png'
+// import iconSix from '../Icons/satellite.png'
+
+export const NavData = [
+  {
+    title: 'Galaxies',
+    path: '/galaxies',
+    cName: 'navItem',
+    // icon: iconOne
+  },
+  {
+    title: 'Astronomical Object',
+    path: '/objects',
+    cName: 'navItem',
+    // icon: iconTwo
+  },
+  {
+    title: 'People',
+    path: '/people',
+    cName: 'navItem',
+    // icon: iconThree
+  },
+  {
+    title: 'Planets',
+    path: '/planets',
+    cName: 'navItem',
+    // icon: iconFour
+  },
+  {
+    title: 'Satellites',
+    path: '/satellites',
+    cName: 'navItem',
+    // icon: iconFive
+  },
+  {
+    title: 'Stars',
+    path: '/stars',
+    cName: 'navItem',
+    // icon: iconSix
+  }
+]
