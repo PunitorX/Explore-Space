e0073e81ef9fe750ccf77e98e3320627a721d6f3
Robert Jones <JonesBrendanDev@gmail.com>
2023-02-07 22:23:52 -0600

    added icons; updated SideNavData

--- frontend/src/data/SideNavData.js
+++ frontend/src/data/SideNavData.js
@@ -1,45 +1,81 @@
-import iconOne from '../icons/009-galaxy-1.png'
-import iconTwo from '../icons/023-comet.png'
-import iconThree from '../icons/035-astronomer.png'
-import iconFour from '../icons/030-planets.png'
-import iconFive from '../icons/047-space-exploration.png'
-import iconSix from '../icons/048-constellation-1.png'
+import iconOne from '../icons/024-earth.png'
+import iconTwo from '../icons/009-galaxy-1.png'
+import iconThree from '../icons/023-comet.png'
+import iconFour from '../icons/035-astronomer.png'
+import iconFive from '../icons/030-planets.png'
+import iconSix from '../icons/moon.png'
+import iconSeven from '../icons/047-space-exploration.png'
+import iconEight from '../icons/048-constellation-1.png'
+import iconNine from '../icons/aliens.png'
+import iconTen from '../icons/001-startup.png'
+import iconEleven from '../icons/astronomy.png'
+
 
 export const NavData = [
+  {
+    title: 'Home',
+    path: '/',
+    cName: 'navItem',
+    icon: iconOne
+  },
   {
     title: 'Galaxies',
     path: '/galaxies',
     cName: 'navItem',
-    icon: iconOne
+    icon: iconTwo
   },
   {
     title: 'Astronomical Objects',
     path: '/objects',
     cName: 'navItem',
-    icon: iconTwo
+    icon: iconThree
   },
   {
     title: 'Astronomers',
-    path: '/Astronomers',
+    path: '/astronomers',
     cName: 'navItem',
-    icon: iconThree
+    icon: iconFour
   },
   {
     title: 'Planets',
     path: '/planets',
     cName: 'navItem',
-    icon: iconFour
+    icon: iconFive
+  },
+  {
+    title: 'Moons',
+    path: '/moons',
+    cName: 'navItem',
+    icon: iconSix
   },
   {
     title: 'Satellites',
     path: '/satellites',
     cName: 'navItem',
-    icon: iconFive
+    icon: iconSeven
   },
   {
     title: 'Stars',
     path: '/stars',
     cName: 'navItem',
-    icon: iconSix
+    icon: iconEight
+  },
+  {
+    title: 'Intelligent Life',
+    path: '/life',
+    cName: 'navItem',
+    icon: iconNine
+  },
+  {
+    title: 'Space Travel',
+    path: '/travel',
+    cName: 'navItem',
+    icon: iconTen
+  },
+  {
+    title: 'Theories',
+    path: '/theories',
+    cName: 'navItem',
+    icon: iconEleven
   }
 ]
