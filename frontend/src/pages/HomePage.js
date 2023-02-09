5ce996bf18d73c5dc1ec94a5a6c96152b5b70bb3
Robert Jones <jones.brendan1996@gmail.com>
2023-02-09 00:49:25 -0600

    added video to homepage

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,8 +1,32 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
+import '../styles/HomePage.css'
+import Video from '../videos/earth.mp4'
 
-const HomePage = () => {
+const HomePage = ({src, isMuted}) => {
+  const refVideo = useRef(null)
+
+  useEffect(() => {
+    if(!refVideo.current) {
+      return
+    }
+
+    if(isMuted) {
+      //open bug since 2017 that you cannot set muted in video element https://github.com/facebook/react/issues/10389
+      refVideo.current.defaultMuted = true
+      refVideo.current.muted = true
+    }
+
+    refVideo.current.srcObject = src
+  }, [src])
+
   return (
-    <div>Home</div>
+    <body className='homePage'>
+      <div className='media'>
+        <video className="mediaContainer" autoPlay ref={refVideo} muted loop>
+          <source src={Video} type='video/mp4'/>
+        </video>
+      </div>
+    </body>
   )
 }
 
