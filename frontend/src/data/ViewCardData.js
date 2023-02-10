36c5f0c0afcea8d947230bf1f8839573a9672476
Robert Jones <JonesBrendanDev@gmail.com>
2023-02-10 16:11:36 -0600

    uploaded photos; created ViewCardData file

---
+++ frontend/src/data/ViewCardData.js
@@ -0,0 +1,63 @@
+import iconOne from '../images/galaxyOne.jpg'
+import iconTwo from '../images/comet.jpg'
+import iconThree from '../images/sagan.jpg'
+import iconFour from '../images/neptune.jpg'
+import iconFive from '../images/moon.jpg'
+import iconSix from '../images/satellite.jpg'
+import iconSeven from '../images/stars.jpg'
+import iconEight from '../images/et.jpg'
+import iconNine from '../images/spacetravel.jpg'
+import iconTen from '../images/theory.jpg'
+
+export const ViewData = [
+    {
+        topic: 'Galaxies',
+        desc: 'Anim eiusmod tempor cupidatat esse Lorem id in. Sit eiusmod reprehenderit laboris consequat ipsum. Incididunt eu pariatur ex laboris veniam est fugiat eu officia dolore.',
+        img: iconOne
+    },
+    {
+        topic: 'Astronomical Objects',
+        desc: 'Anim eiusmod tempor cupidatat esse Lorem id in. Sit eiusmod reprehenderit laboris consequat ipsum. Incididunt eu pariatur ex laboris veniam est fugiat eu officia dolore.',
+        img: iconTwo
+    },
+    {
+        topic: 'Astronomers',
+        desc: 'Anim eiusmod tempor cupidatat esse Lorem id in. Sit eiusmod reprehenderit laboris consequat ipsum. Incididunt eu pariatur ex laboris veniam est fugiat eu officia dolore.',
+        img: iconThree
+    },
+    {
+        topic: 'Planets',
+        desc: 'Anim eiusmod tempor cupidatat esse Lorem id in. Sit eiusmod reprehenderit laboris consequat ipsum. Incididunt eu pariatur ex laboris veniam est fugiat eu officia dolore.',
+        img: iconFour
+    },
+    {
+        topic: 'Moons',
+        desc: 'Anim eiusmod tempor cupidatat esse Lorem id in. Sit eiusmod reprehenderit laboris consequat ipsum. Incididunt eu pariatur ex laboris veniam est fugiat eu officia dolore.',
+        img: iconFive
+    },
+    {
+        topic: 'Satellites',
+        desc: 'Anim eiusmod tempor cupidatat esse Lorem id in. Sit eiusmod reprehenderit laboris consequat ipsum. Incididunt eu pariatur ex laboris veniam est fugiat eu officia dolore.',
+        img: iconSix
+    },
+    {
+        topic: 'Stars',
+        desc: 'Anim eiusmod tempor cupidatat esse Lorem id in. Sit eiusmod reprehenderit laboris consequat ipsum. Incididunt eu pariatur ex laboris veniam est fugiat eu officia dolore.',
+        img: iconSeven
+    },
+    {
+        topic: 'Intelligent Life',
+        desc: 'Anim eiusmod tempor cupidatat esse Lorem id in. Sit eiusmod reprehenderit laboris consequat ipsum. Incididunt eu pariatur ex laboris veniam est fugiat eu officia dolore.',
+        img: iconEight
+    },
+    {
+        topic: 'Space Travel',
+        desc: 'Anim eiusmod tempor cupidatat esse Lorem id in. Sit eiusmod reprehenderit laboris consequat ipsum. Incididunt eu pariatur ex laboris veniam est fugiat eu officia dolore.',
+        img: iconNine
+    },
+    {
+        topic: 'Theories',
+        desc: 'Anim eiusmod tempor cupidatat esse Lorem id in. Sit eiusmod reprehenderit laboris consequat ipsum. Incididunt eu pariatur ex laboris veniam est fugiat eu officia dolore.',
+        img: iconTen
+    },
+]
